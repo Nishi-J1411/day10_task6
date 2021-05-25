c484eda4fd54ca0e63d3c07b136ca847d31fe996
unknown <nishij052@gmail.com>
2021-05-25 19:49:55 +0530

    first commit

---
+++ get_phone_no.js
@@ -0,0 +1,7 @@
+// $('.modal-auto-clear').on('shown.bs.modal', function () {
+//     // if data-timer attribute is set use that, otherwise use default (7000)
+//     var timer = $(this).data('timer') ? $(this).data('timer') : 7000;
+//     $(this).delay(timer).fadeOut(200, function () {
+//         $(this).modal('hide');
+//     });
+// })
